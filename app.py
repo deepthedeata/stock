6746ce41112679de79efccfb7de0275048eb13a9
deepthedeata <98915818+deepthedeata@users.noreply.github.com>
2022-10-11 15:58:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,9 @@ Created on Tue Oct 11 15:14:43 2022
 import streamlit as st
 from datetime import date
 import yfinance as yf
-from fbprophet import Prophet
-from pyplot import graph_objs as go
+from prophet import Prophet
+from prophet.plot import plot_plotly
+from plotly import graph_objs as go
 
 
 START = "2015-01-01"
@@ -17,7 +18,7 @@ TODAY = date.today().strftime("%Y-%m-%d")
 st.title("Stock Prediction App")
 
 stocks = ("AAPL","GOOG","MSFT","GME")
-selecte_stocks= st.selectbox("Select dataset for prediction",stocks)
+selected_stocks= st.selectbox("Select dataset for prediction",stocks)
 
 n_years = st.slider("Years of prediction:",1,4)
 period = n_years*365
